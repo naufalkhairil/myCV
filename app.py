898d3eadb62007a1a35a7b473c556371a71aa7c9
naufalkhairil <naufalimami@gmail.com>
2020-08-13 17:51:14 +0700

    edit resume section

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask,render_template,url_for
+import os
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder= os.path.join(os.getcwd(),'static'))
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
